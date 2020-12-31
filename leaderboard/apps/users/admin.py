994e2446d2b42fd931e60bf7681f8838321f56d7
thomasrea0113 <thomasrea0113@gmail.com>
2020-12-31 06:20:27 +0000

    add user admin

--- leaderboard/apps/users/admin.py
+++ leaderboard/apps/users/admin.py
@@ -1,5 +1,39 @@
+
+from typing import TYPE_CHECKING
 from django.contrib import admin
+from django.template.loader import render_to_string
 from django.contrib.auth.admin import UserAdmin
 from .models import AppUser
 
-admin.site.register(AppUser, UserAdmin)
+if TYPE_CHECKING:
+    from typing import Optional
+    from django.http import HttpRequest
+    from django.db.models.options import Options
+
+
+@admin.register(AppUser)
+class AppUserAdmin(UserAdmin):
+    list_display = ['username', 'email', 'first_name',
+                    'last_name', 'gender', 'is_staff', 'birthday', 'weight']
+    list_filter = ['is_staff', 'is_superuser', 'is_active', 'groups', 'gender']
+
+    readonly_fields = ['eligable_weight_classes']
+
+    def eligable_weight_classes(self, user: 'AppUser'):
+        query_set = user.get_eligable_weight_classes()
+        meta: 'Options' = query_set.model._meta
+        url_name = f'{self.admin_site.name}:{meta.app_label}_{meta.model_name}_change'
+
+        return render_to_string('admin/includes/model_link_list.html', context={
+            'url_name': url_name, 'query_set': query_set,
+            'admin_site': self.admin_site})
+
+    def get_fieldsets(self, request: 'HttpRequest', obj: 'Optional[AppUser]' = None):
+        # typing is incorrect in the current version of the stubs
+        return super().get_fieldsets(request, obj) + (
+            ('Eligibility', {'fields': ('eligable_weight_classes',)}),
+        )  # type: ignore[reportGeneralTypeIssues]
+
+    def changelist_view(self, request, extra_context=None):
+        (extra_context := extra_context or {}).update(has_multiselect=True)
+        return super().changelist_view(request, extra_context=extra_context)
--- leaderboard/apps/users/models.py
+++ leaderboard/apps/users/models.py
@@ -1,6 +1,7 @@
-from django.utils.translation import gettext_lazy as _
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.db.models.query_utils import Q
+from django.utils.translation import gettext_lazy as _
 
 from apps.users.managers import AppUserManager
 
@@ -18,3 +19,23 @@ class AppUser(AbstractUser):
         max_digits=5, decimal_places=2, null=True, blank=True)
 
     objects = AppUserManager()
+
+    def get_eligable_weight_classes(self):
+        # TODO Genders being in the user models module is causing a circular reference.
+        # move genders to separate module caused the appUser module to appear as not registered
+        # pylint: disable=import-outside-toplevel
+        from apps.divisions.models import WeightClass
+
+        filtered = WeightClass.objects.get_queryset()
+
+        # TODO finish filtering
+        if self.weight is not None:
+            # for weight classes with both bounds
+            filtered = filtered.filter(
+                Q(lower_bound__gt=0, upper_bound__gt=0) &
+                Q(lower_bound__lte=self.weight, upper_bound__gt=self.weight))
+        else:
+            # user has no weight, only eligble for weight classes with no bounds specified
+            filtered = filtered.filter(lower_bound__eq=0, upper_bound__eq=0)
+
+        return filtered
