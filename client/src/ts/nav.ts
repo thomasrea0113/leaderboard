064d1217ad18cf4107b43ed8f99a134b9e21d9e0
thomasrea0113 <thomasrea0113@gmail.com>
2021-01-12 20:02:59 +0000

    fix mobile nav

--- client/src/ts/nav.ts
+++ client/src/ts/nav.ts
@@ -3,19 +3,15 @@
  * disable pointer events during a transition
  */
 export const initializeNav = (): void => {
-  document.querySelectorAll('.dropdown').forEach((dropDown) =>
-    dropDown.addEventListener('mouseenter', () =>
-      dropDown.querySelectorAll('.dropdown-menu').forEach((menu) => {
-        menu.classList.add('transitioning');
-        console.log('adding transition');
-      }),
-    ),
-  );
+  document
+    .querySelectorAll('.dropdown')
+    .forEach((dropDown) =>
+      dropDown.addEventListener('mouseenter', () =>
+        dropDown.querySelectorAll('.dropdown-menu').forEach((menu) => menu.classList.add('transitioning')),
+      ),
+    );
 
-  document.querySelectorAll('.dropdown-menu').forEach((menu) =>
-    menu.addEventListener('transitionend', () => {
-      console.log('removing transition');
-      return menu.classList.remove('transitioning');
-    }),
-  );
+  document
+    .querySelectorAll('.dropdown-menu')
+    .forEach((menu) => menu.addEventListener('transitionend', () => menu.classList.remove('transitioning')));
 };
